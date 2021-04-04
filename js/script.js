01bbe2c27a771f495493c53e1e46753f97536a44
bael <osh.kg1819@gmail.com>
2021-04-04 14:50:24 +0600

    The first save

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+let toggle = document.querySelector(".toggle")
+let header_upper = document.querySelector(".header_upper")
+
+toggle.onclick = () => {
+    header_upper.classList.toggle("left")
+}
